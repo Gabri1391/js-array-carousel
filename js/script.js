c5d50cfa401ad23bb74c49c843f0459f0bfabf76
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-27 17:29:20 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const imagesList = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
+const gallery = document.getElementById('gallery');
+
+
+let currentActiveIndex = 0;
+
+imagesList[currentActiveIndex].classList.add('active');
 
 //Ciclo for per stampare tutti gli elementi dell'array
-for(i = 0; i < imagesList.lenght; i++){
-    console.log(imagesList[i]);
+for( let i = 0; i < imagesList.length; i++){
+    gallery.innerHTML = `<img src="${imagesList[0]}" alt="immagine${imagesList[i + 1]}"</img> <img src="${imagesList[1]}" alt="immagine${imagesList[i + 1]}"</img> <img src="${imagesList[2]}" alt="immagine${imagesList[i + 2]}"</img> <img src="${imagesList[3]}" alt="immagine${imagesList[i + 3]}"</img> <img src="${imagesList[4]}" alt="immagine${imagesList[i + 4]}"</img>`
+
 }
