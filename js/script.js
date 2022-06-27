4986ca0e2ec0d5869381e0f80e05b19b5b365f3d
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-27 18:47:42 +0200

    milestone 3 + bonus 1 done

--- js/script.js
+++ js/script.js
@@ -10,6 +10,36 @@ for( let i = 0; i < imagesList.length; i++){
     gallery.innerHTML += `<img src="${imagesList[i]}" class="img-item" alt="immagine${imagesList[i + 1]}" />`;
 }
 
+//Creo una costante per accomunare tutte le immagini 
 const images = document.getElementsByClassName('img-item');
 
 images[currentActiveIndex].classList.add('active');
+
+//Creo la logica dei bottoni per cambiare dinamicamente le immagini
+
+const previous = document.getElementById('previous');
+const next = document.getElementById('next');
+
+next.addEventListener('click', function(){
+    images[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex += 1;
+
+    if(currentActiveIndex === images.length){
+        currentActiveIndex = 0;
+    }
+
+    images[currentActiveIndex].classList.add('active');
+})
+
+previous.addEventListener('click', function(){
+    images[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex += -1;
+
+    if(currentActiveIndex = 0){
+        currentActiveIndex === images.length;
+    }
+    
+    images[currentActiveIndex].classList.add('active');
+})
