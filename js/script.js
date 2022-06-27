c12bd14b0a96f30d078690f1c09458a9cb442500
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-27 16:37:55 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const imagesList = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+
+
+//Ciclo for per stampare tutti gli elementi dell'array
+for(i = 0; i < imagesList.lenght; i++){
+    console.log(imagesList[i]);
+}
