c139fa33409fbf3d4f7fbed2a4c79058e6959c71
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-28 17:30:43 +0200

    fixed carousel & added thumbnails

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-const imagesList = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
+const gallery = document.querySelector('#carousel-section .gallery');
+const thumbnails = document.getElementById('thumbnails');
+const sources = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-const gallery = document.getElementById('gallery');
-
-
-let currentActiveIndex = 0;
-
-//Ciclo for per stampare tutti gli elementi dell'array
-for( let i = 0; i < imagesList.length; i++){
-    gallery.innerHTML += `<img src="${imagesList[i]}" class="img-item" alt="immagine${imagesList[i + 1]}" />`;
+let imageElements = ''; 
+for( let i = 0; i < sources.length; i++){
+    imageElements += `<img src="${sources[i]}" alt="immagine-${sources[i + 1]}" />`;
 }
 
-//Creo una costante per accomunare tutte le immagini 
-const images = document.getElementsByClassName('img-item');
+gallery.innerHTML = imageElements;
+thumbnails.innerHTML = imageElements;
 
+//Recupero tutte le immagini dal DOM 
+const images = document.querySelectorAll('#carousel-section img');
+
+//Variabile per indicare l'indice attivo
+let currentActiveIndex = 0;
+
+//Recupero le thumbs dal DOM
+const thumbs = document.querySelectorAll('#thumbnails img');
+
+//Imposto alla prima immagine la classe active
 images[currentActiveIndex].classList.add('active');
+
+//Imposto alla prima thumbs la classe active
+thumbs[currentActiveIndex].classList.add('active');
 
 //Creo la logica dei bottoni per cambiare dinamicamente le immagini
 
@@ -22,24 +32,29 @@ const next = document.getElementById('next');
 
 next.addEventListener('click', function(){
     images[currentActiveIndex].classList.remove('active');
+    thumbs[currentActiveIndex].classList.remove('active');
 
-    currentActiveIndex += 1;
+    currentActiveIndex++;
 
     if(currentActiveIndex === images.length){
         currentActiveIndex = 0;
     }
+    console.log(currentActiveIndex)
 
     images[currentActiveIndex].classList.add('active');
+    thumbs[currentActiveIndex].classList.add('active');
 })
 
 previous.addEventListener('click', function(){
     images[currentActiveIndex].classList.remove('active');
+    thumbs[currentActiveIndex].classList.remove('active');
 
-    currentActiveIndex += -1;
+    currentActiveIndex--;
 
-    if(currentActiveIndex = 0){
-        currentActiveIndex === images.length;
-    }
-    
+   if(currentActiveIndex < 0){
+    currentActiveIndex = images.length -1;
+   }
+
     images[currentActiveIndex].classList.add('active');
+    thumbs[currentActiveIndex].classList.add('active');
 })
